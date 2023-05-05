f41b0f67e713e8aa9884a3f59d0c3e2e52ece3f9
shanliu <shan.liu@msn.com>
2023-05-05 21:32:52 +0800

    Update book.js

--- book.js
+++ book.js
@@ -1,3 +1,5 @@
+
+document.body.innerHTML='<p style="text-align: center;font-size: 1rem; position: absolute; width: 100%;top: 50%;">前往 企业内部应用开放平台 中 , 请稍后...</p>';
 if(window.location.pathname.indexOf("/ui.html")==0){
     window.location.href="http://175.178.90.181/ui/"
 }
