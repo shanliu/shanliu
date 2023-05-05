a7cdbda5e770aa83ef9dcb44b04f7c0b92cd516a
shanliu <shan.liu@msn.com>
2023-05-05 21:36:04 +0800

    Update lsys.js

--- lsys.js
+++ lsys.js
@@ -1,5 +1,4 @@
-
-document.body.innerHTML='<p style="text-align: center;font-size: 1rem; position: absolute; width: 100%;top: 50%;">前往 企业内部应用开放平台 中 , 请稍后...</p>';
 if(window.location.pathname.indexOf("/ui.html")==0){
+    document.body.innerHTML='<p style="text-align: center;font-size: 1rem; position: absolute; width: 100%;top: 50%;">前往 企业内部应用开放平台 中 , 请稍后...</p>';
     window.location.href="http://175.178.90.181/ui/"
 }
