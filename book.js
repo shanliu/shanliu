5bc7245c0e3ea0161f2b2c25bc5ecc6e20c339e7
shanliu <shan.liu@msn.com>
2023-05-05 13:16:08 +0000

    Deploying to gh-pages from @ shanliu/shanliu@464e598b008cd6d9282007e4a5d97a4c8187c418 🚀

--- book.js
+++ book.js
@@ -1,5 +1,3 @@
-(function(){
-  if(window.location.pathname.indexOf("/ui.html")==0){
-      window.location.href="http://175.178.90.181/ui/"
-  }
-})()
+if(window.location.pathname.indexOf("/ui.html")==0){
+    window.location.href="http://175.178.90.181/ui/"
+}
